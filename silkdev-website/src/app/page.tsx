0ad6b9e5ac8fbf5cc5ee066b77dc93e1a9f4df95
gharradhaifa <gharradhaifa6@gmail.com>
2023-09-25 22:02:44 +0100

    reviews

--- silkdev-website/src/app/page.tsx
+++ silkdev-website/src/app/page.tsx
@@ -3,12 +3,14 @@ import HeroSection from './components/hero/HeroSection'
 import SideBar from './components/sidebar/SideBar'
 import OurServices from './components/OurServices/OurServices'
 import Portfolio from './components/projectSection/Portfolio'
+import OurBlog from './components/OurBlog/OurBlog'
 export default function Home() {
   return (
     <>
      <HeroSection />
      <OurServices />
      <Portfolio />
+     <OurBlog/>
     </>
   )
 }
