b23db15194995d9f6091864510267b032a7924a9
gharradhaifa <gharradhaifa6@gmail.com>
2023-09-29 21:14:12 +0100

    filter img

--- silkdev-website/src/app/components/OurServices/OurServices.tsx
+++ silkdev-website/src/app/components/OurServices/OurServices.tsx
@@ -9,7 +9,7 @@ const OurServices = () => {
 
   return (
     <div>
-      <div className={styles.container}>
+<div className={styles.container}>
       <h1 className={styles.containerh1}>Our Services</h1>
       <p className={styles.containerp}>
         As a tight-knit team of experts, we create<br/>
@@ -20,26 +20,18 @@ const OurServices = () => {
         <div className={styles.cardscontainer}>
       <div className={styles.card}>
         
-        <Image className={styles.cardimg}
+        <Image className={`${styles.cardimg} ${styles['black-overlay']}`}
           src={img1}
           alt=""
         />
         <div className={styles['card-content']}>
           <h2>Web developement</h2>
-          {/* <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste, voluptatum, quia explicabo
-            laboriosam rem adipisci voluptates cumque, veritatis atque nostrum corrupti ipsa asperiores harum? Dicta odio aut
-            hic.
-          </p> */}
-          {/* <a href="#" className={styles.button}>
-            Find out more
-            <span className="material-symbols-outlined">arrow_right_alt</span>
-          </a> */}
+          
         </div>
       </div>
       <div className={styles.card}>
         
-      <Image className={styles.img}
+      <Image className={`${styles.cardimg} ${styles['black-overlay']}`}
           src={img3}
           alt=""
         />
@@ -58,7 +50,7 @@ const OurServices = () => {
       </div>
       <div className={styles.card}>
         
-      <Image className={styles.img}
+      <Image className={`${styles.cardimg} ${styles['black-overlay']}`}
           src={img2}
           alt=""
         />
